b48ff102fa6c5694219c4e1b089bb349d5a31a0f
AlexandraArdelean <ale.xandra91@yahoo.com>
2021-06-20 10:40:57 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackit/course3/model/Car.java
@@ -0,0 +1,49 @@
+package org.fasttrackit.course3.model;
+
+public class Car {
+    private String brand;
+    private int numberOfDoors;
+    private Integer numberOfWheels;
+    private String carColor;
+    private int carWeight;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public Integer getNumberOfWheels() {
+        return numberOfWheels;
+    }
+
+    public void setNumberOfWheels(Integer numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public int getCarWeight() {
+        return carWeight;
+    }
+
+    public void setCarWeight(int carWeight) {
+        this.carWeight = carWeight;
+    }
+}
