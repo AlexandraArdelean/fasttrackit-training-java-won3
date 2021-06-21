0c71f1bb64f654838657f15e0f4a5ca0d5c18afc
AlexandraArdelean <ale.xandra91@yahoo.com>
2021-06-21 19:36:33 +0300

    [Course 4] Arithmetic Operators

---
+++ src/course4/calculator/Main.java
@@ -0,0 +1,10 @@
+package course4.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        CalculatorOp op = new CalculatorOp();
+
+        op.switchMethod(58, 89, "sum");
+
+    }
+}
