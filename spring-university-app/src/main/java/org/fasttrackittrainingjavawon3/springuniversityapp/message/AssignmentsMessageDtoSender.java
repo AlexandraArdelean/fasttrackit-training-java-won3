aee28d8665408294a0729a0628df45dd73e79351
AlexandraArdelean <ale.xandra91@yahoo.com>
2021-10-24 14:27:08 +0300

    [spring-university-app] - updates-added a readme.md

---
+++ spring-university-app/src/main/java/org/fasttrackittrainingjavawon3/springuniversityapp/message/AssignmentsMessageDtoSender.java
@@ -0,0 +1,42 @@
+package org.fasttrackittrainingjavawon3.springuniversityapp.message;
+
+
+
+public class AssignmentsMessageDtoSender {
+    private Long schedulesId;
+    private String universityDepartment;
+    private String universityYear;
+    private String semesterNumber;
+
+    public Long getSchedulesId() {
+        return schedulesId;
+    }
+
+    public void setSchedulesId(Long schedulesId) {
+        this.schedulesId = schedulesId;
+    }
+
+    public String getUniversityDepartment() {
+        return universityDepartment;
+    }
+
+    public void setUniversityDepartment(String universityDepartment) {
+        this.universityDepartment = universityDepartment;
+    }
+
+    public String getUniversityYear() {
+        return universityYear;
+    }
+
+    public void setUniversityYear(String universityYear) {
+        this.universityYear = universityYear;
+    }
+
+    public String getSemesterNumber() {
+        return semesterNumber;
+    }
+
+    public void setSemesterNumber(String semesterNumber) {
+        this.semesterNumber = semesterNumber;
+    }
+}
